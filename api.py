78020bb3d59a2a49ff4364b0e1b1d932b8164efb
Galc3882 <86870298+Galc3882@users.noreply.github.com>
2022-06-12 14:18:08 -0400

    Fixed url key parsing, added gitignore

--- api.py
+++ api.py
@@ -1,4 +1,3 @@
-import os
 from django.http import HttpResponse
 import flask
 from flask import request, jsonify
@@ -21,6 +20,7 @@ def home():
 
 @app.route('/api/songs', methods=['GET'])
 def songName():
+    global database
     # Check if an name was provided as part of the URL.
     # If not, then return an error in the HTTP response.
     if 'name' in request.args:
@@ -28,15 +28,28 @@ def songName():
     else:
         return "Error: No name field provided. Please specify an name."
 
-    
+    '''
+    Parse url variables and return a list of songs
+        :	Separate protocol (http) from address	%3B
+        /	Separate domain and directories	%2F
+        #	Separate anchors	%23
+        ?	Separate query string	%3F
+        &	Separate query elements	%24
+        @	Separate username and password from domain	%40
+        %	Indicates an encoded character	%25
+        +	Indicates a space	%2B
+        <space>	Not recommended in URLs	%20 or +
+    '''
     return jsonify(Search.fuzzyGetSongTitle(name, database.keys(), threshold=40))
 
 @app.route('/api/recommend', methods=['GET'])
 def songRecommendation():
+    global database
     # Check if an key was provided as part of the URL.
     # If not, then return an error in the HTTP response.
     if 'key' in request.args:
         key = request.args['key']
+        key = key.replace('\\u0000', '\0') 
     else:
         return "Error: No key field provided. Please specify an key."
 
@@ -54,14 +67,9 @@ def takeSecond(elem):
 
 
 if __name__ == '__main__':
-    # get request count and initialize database if it doesn't exist
-    i = int(os.environ.get('TIMES', 1))
-    if i == 1:
-        with open('database.pickle', 'rb') as handle:
-            database = pickle.load(handle)
-        
-    # update request count
-    os.environ['TIMES'] = str(i+1)
+    with open('database.pickle', 'rb') as handle:
+        global database
+        database = pickle.load(handle)
 
     #HttpResponse('Hello! ' * times)
     app.run()
