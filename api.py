48636de67b857210ebffad95fc9a67cb8812befd
Galc3882 <86870298+Galc3882@users.noreply.github.com>
2022-07-13 22:35:38 -0400

    Update api.py

--- api.py
+++ api.py
@@ -27,8 +27,9 @@ class MyWorker():
         This function loads the database
         '''
         # delete all the files in the folder tmp
-        for file in os.listdir("tmp"):
-            os.remove("tmp/" + file)
+        if os.path.exists('tmp'):
+            for file in os.listdir("tmp"):
+                os.remove("tmp/" + file)
 
         root = os.path.abspath(os.getcwd()) + r'/tmp'
         # create temp folder
